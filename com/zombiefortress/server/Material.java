f01ff0a317d51fb5e811b25c295912c4c1cf873e
Oisin100 <minedudes2012@gmail.com>
2015-06-14 17:23:03 +0200

    Updates/Changes
    Added Door, Changed Material And world

--- com/zombiefortress/server/Material.java
+++ com/zombiefortress/server/Material.java
@@ -4,9 +4,10 @@ public interface Material {
 	int GRASS = 1;
 	int CONCRETE_FLOOR = 2;
 	int CONCRETE_WALL = 3;
-	int WOODEN_BARRICADE = 4;
-	int REINFORCED_BARRICADE = 5;
-	int GLASS_WINDOW = 6;
-	int REINFORCED_WINDOW = 7;
+	int DOOR = 4;
+	int WOODEN_BARRICADE = 5;
+	int REINFORCED_BARRICADE = 6;
+	int GLASS_WINDOW = 7;
+	int REINFORCED_WINDOW = 8;
 	
 }
