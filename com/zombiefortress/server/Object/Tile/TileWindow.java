ca60127158f0b0febd7b7fb1748e94a4971d17f4
Oisin100 <oisin100@users.noreply.github.com>
2015-06-16 14:21:46 +0200

    Update TileWindow.java

--- com/zombiefortress/server/Object/Tile/TileWindow.java
+++ com/zombiefortress/server/Object/Tile/TileWindow.java
@@ -1,7 +1,9 @@
 package com.zombiefortress.server.Object.Tile;
-+
-+public class TileWindow extends TileObject{
-+
-+ public TileWindow(int X, int Y) {
-+  super(X, Y, 6);
-+ }
+
+public class TileWindow extends TileObject{
+
+ public TileWindow(int X, int Y) {
+  super(X, Y, 6);
+ }
+ 
+}
